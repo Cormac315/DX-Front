325a08a5459292b9189a487a9ce50e2d9950e1a6
Cormac <1350363682@qq.com>
2025-05-13 17:30:05 +0800

    站点标题

--- src/main.js
+++ src/main.js
@@ -8,4 +8,23 @@ const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(router)
+
+// 设置动态标题
+router.beforeEach((to, from, next) => {
+  let title = '洞玄'
+  switch (to.path) {
+    case '/':
+      title += ' - 主页'
+      break
+    case '/projects':
+      title += ' - 我的项目'
+      break
+    case '/settings':
+      title += ' - 设置'
+      break
+  }
+  document.title = title
+  next()
+})
+
 app.mount('#app') 
